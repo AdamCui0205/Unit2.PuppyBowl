ea7658e6467581d3fa962538b57644ba5754aecc
Peng Cui <pengcui@Pengs-MBP.lan>
2023-11-09 13:47:39 -0800

    classend Finished so far

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ const state = {
 
 //get access all data from API
 const getAllPlayers = async() =>{
+try{
     const response = await fetch(`${basicUrl}players`);
     const responsejson = await response.json();
     state.allPlayers = responsejson.data.players;
-    //console.log(state.allPlayers)
+} catch(err){
+    console.log(err);
+    console.log(`There was an error with your promise`);
+}
+console.log(state.allPlayers)
     renderAllPlayers();
 }
 
@@ -20,20 +25,20 @@ const getAllPlayers = async() =>{
 const getPlayerDetails = async(id) => {
     const response = await fetch(`${basicUrl}players/${id}`)
     const responsejson = await response.json();
-    console.log(responsejson);
     const playerDetails = responsejson.data.player;
     renderDetails(playerDetails);
 }
 
+
 //create a renderDetails to build up transfer page HTML content, include breed, and img
 const renderDetails = (detailsOfAPlayer) => {
     const html = `
-    <section>
+    <div>
     <h2>${detailsOfAPlayer.name}</h2>
     
-    <img src="${detailsOfAPlayer.imageUrl}" alt="image of a player"/>
-    
-    <P>${detailsOfAPlayer.breed}</P></section>
+    <P>${detailsOfAPlayer.status}</P>
+
+    <P>${detailsOfAPlayer.breed}</P></div>
     
     <button id="back-button">Go Back To List</button>
     `;
@@ -45,13 +50,15 @@ const renderDetails = (detailsOfAPlayer) => {
         renderAllPlayers();
     })
 }
-
+//<img src="${detailsOfAPlayer.imageUrl}" alt="image of a player"/>
 
 
 //get access all names from data, and render list for names
 const renderAllPlayers = () => {
     const playerNames = state.allPlayers.map((singlePlayer) =>{
-    return `<li id="${singlePlayer.id}">${singlePlayer.name}</li>`;
+    return `<section><li>${singlePlayer.name}</li>
+    <img id="${singlePlayer.id}" src="${singlePlayer.imageUrl}" alt="image of a player"/>
+    <button>Delete</button></section>`;
     }) //[`<li>Name of the recipe</li>]
 
     //create a ol,add list into ol, and append ol under main part
@@ -60,47 +67,54 @@ const renderAllPlayers = () => {
     main.replaceChildren(ol);
 
     //create a function, create a interaction when click the players' name, for future use
-    const listPlayers = document.querySelectorAll('li');
+    const listPlayers = document.querySelectorAll('img');
     listPlayers.forEach((singlePlayerList) => {
+        console.log(singlePlayerList);
         singlePlayerList.addEventListener('click',
         (event) => {
             getPlayerDetails(event.target.id)
         })
     })
 }
+//invoke the function
 getAllPlayers();
 
+//design the form, collect input of the form
+const form = document.querySelector(`form`);
+form.addEventListener(`submit`, async(event) => {
+    event.preventDefault();
 
+    const nameInput = document.querySelector(`#name`);
+    const imageUrlInput = document.querySelector(`#img-Url`);
+    const breedInput = document.querySelector(`#breed`);
 
+    const response = await fetch(`${basicUrl}players`, {
+        method: `POST`,
+        headers: {
+            "Content-Type": `application/json`,
+        },
+        body: JSON.stringify({
+            name: nameInput.value,
+            imageUrl: imageUrlInput.value,
+            breed: breedInput.value
+        })
+    });
+    const newPlayer = await response.json();
+    console.log(newPlayer);
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-//create a renderDetails to build up transfer page HTML content, include breed, and img
-// const renderDetails = (detailsOfAPlayer) => {
-//     const html = `
-//     <h2>${detailsOfAPlayer.name}</h2>
-    
-//     <img src="${detailsOfAPlayer.imageUrl}" alt="image of a player"/>
-    
-//     <P>${detailsOfAPlayer.breed}</P>
-    
-//     <button id="back-button">Go Back To List</button>
-//     `;
-//     main.innerHTML = html;
-//     //set up button, when we click button, will go back to first outlook
-//     const backButton = document.querySelector(`#back-button`);
-//     console.log(backButton)
-//     backButton.addEventListener('click', (event)=>{
-//         renderAllPlayers();
-//     })
-// }
+//Delete item;
+const section = document.querySelector(`section`);
+section.addEventListener(`Delete`, async(event) => {
+    event.preventDefault();
+    const response = await fetch(`${basicUrl}players`, {
+        method: `Delete`,
+        body: JSON.stringify({
+            name: nameInput.value,
+            imageUrl: imageUrlInput.value,
+            breed: breedInput.value
+        })
+    });
+    const newPlayer = await response.json();
+    console.log(newPlayer);
+})
